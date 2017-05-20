9c32eaac0d68a50faa436ccc1c79828b0c8a5ac9
Eva Lai <evalai.dc@gmail.com>
2017-05-20 14:38:42 -0400

    closures

--- file.js
+++ file.js
@@ -1,31 +1,29 @@
-//  Global Scope & Shadowing
+//  CLOSURES
 
-// -- note taken from nodeschool.io - scope-chains-closures module
-// The javascript runtime follows these steps to assign a variable:
+//  they are what enalbe callback-last programming - mechanism for handlingt eh asynchronous nature of most JS tasks.
+
+// example from nodeschool.io
+// Given how nesting scope works, it's possible for an inner scope within
+// someFunc() to access bar. In this example, let's say inner() accesses
+// bar:
 //
-//  1) Search within the current scope.
-//  2) If not found, search in the immediately outer scope.
-//  3) If found, go to 6.
-//  4) If not found, repeat 2. Until the Global Scope is reached.
-//  5) If not found in Global Scope, create it (on window / global objects).
-//  6) Assign the value.
-
-
-
-// quux - example of a variable assumed to be in the outer scope.
-// window.quux / global.quux
-// thus allowing for the variable to altered - while the intention was to only allow change in the zip function.
+//     someFunc()
+//      var bar
+//         ↑
+//         |
+//      inner()
+//     alert(bar)
+//         ↑
+//         ⋮
+//
+// Then inner() is said to Close Over bar. Therefore inner() is a Closure.
 
 function foo() {
   var bar;
-  // global quux
   quux = 9;
+  return zip;
   function zip(){
-    // shadow quux of the globally scoped one.
-    var quux = 7;
+    var quux;
+    bar = true;
   };
 };
-
-
-// shadowing - can occur with nested function - variable in the inner function can be the same as outer function but have a different value.
-// the inner variable would be the shadow.
