74e9f82404fd73109314c54e5231ee6234aeed87
Eva Lai <evalai.dc@gmail.com>
2017-05-20 13:53:41 -0400

    block and lexical scope notes

--- file.js
+++ file.js
@@ -1,4 +1,25 @@
 // intro to scope
+// ### Lexical Scope
+// var is used to denote a variable which is Lexically Scoped to the current
+// function:
+//
+//     function someFunc() {
+//       var aVariable;
+//     }
+//
+// aVariable is lexically scoped within someFunc
+
+// ### Block Scope
+//
+// let & const are used to denote variables which are Block Scoped to the
+// current curly braced block:
+//
+//     if (true) {
+//       let aVariable;
+//     }
+//
+// aVariable is block scoped within the if's curly braces
+//
 
 function foo() {
   var bar;
